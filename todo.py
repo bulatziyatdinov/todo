1a09e498208b5de8daf76f6368968928a48ebfc4
ziyatdinov <bulat.ziyatdinov.05@mail.ru>
2026-06-18 22:19:09 +0300

    feat: add support for command line arguments

--- todo.py
+++ todo.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
+import sys
 import time
 from pathlib import Path
 
@@ -32,37 +34,62 @@ class TodoInfo:
                 f"{self.line_str.strip().decode('utf-8', errors='replace')}")
 
 
-def main():
-    found_todos = []
-    error_list = []
-    
-    start_path = '.'
-    start_time = time.perf_counter()
-    
-    for root, dirs, files in os.walk(start_path):
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Search for TODO comments in files.",
+        add_help=True
+    )
+    parser.add_argument('startpath', nargs='?', default='.',
+                        help="Path to file or directory (default: current directory)")
+    return parser.parse_args()
+
+
+def process_file(filepath: Path) -> list[TodoInfo]:
+    todos = []
+    try:
+        with open(filepath, 'rb') as f:
+            file_text = f.readlines()
+        for num, line in enumerate(file_text, 1):
+            if TODO_PREFIX in line:
+                todos.append(TodoInfo(filepath, num, line))
+    except OSError:
+        pass
+    return todos
+
+
+def process_directory(root_path: Path) -> list[TodoInfo]:
+    todos = []
+    for root, dirs, files in os.walk(root_path):
         dirs[:] = [d for d in dirs if d not in EXCLUDED_FOLDERNAMES_LIST]
-        
         for file in files:
             if file not in EXCLUDED_FILENAMES_LIST:
                 filepath = Path(root) / file
-                try:
-                    with open(filepath, 'rb') as f:
-                        file_text = f.readlines()
-
-                    for num, line in enumerate(file_text, 1):
-                        if TODO_PREFIX in line:
-                            found_todos.append(TodoInfo(filepath, num, line))
-
-                except OSError:
-                    error_list.append(f"Can't read '{filepath}")
+                todos.extend(process_file(filepath))
+    return todos
 
 
-    total_time = time.perf_counter() - start_time
+def main():
+    start_time = time.perf_counter()
 
-    print(f"Total TODO count: {len(found_todos)} | Time: {total_time * 1000:.3f} ms\n"
-          + '-' * 40)
+    args = parse_args()
+
+    start_path = Path(args.startpath)
+    
+    if not start_path.exists():
+        print(f'[ERROR] File or directory "{start_path}" does not exist')
+        sys.exit(1)
+    
+    if start_path.is_file():
+        found_todos = process_file(start_path)
+    else:
+        found_todos = process_directory(start_path)
+
     for todo in found_todos:
         print(todo)
+
+    total_time = time.perf_counter() - start_time
+    print('-' * 40 +
+          f'\nTotal TODO count: {len(found_todos)} | Time: {total_time * 1000:.3f} ms')
 
 
 if __name__ == '__main__':
